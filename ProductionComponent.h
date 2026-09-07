6b1e663ead381fae566f28c92b1fb7e90b6f2e16
KP23545756 <162136507+KP23545756@users.noreply.github.com>
2026-09-07 11:09:05 +0200

    Added changes for new iterator class

---
+++ ProductionComponent.h
@@ -0,0 +1,30 @@
+#ifndef PRODUCTIONCOMPONENT_H
+#define PRODUCTIONCOMPONENT_H
+
+#include <string>
+
+class Iterator; // Added for access to vector
+
+class ProductionComponent
+{
+protected:
+  std::string name;
+
+public:
+  ProductionComponent(const std::string &name) : name(name) {};
+
+  virtual ~ProductionComponent() {};
+
+  std::string getName() const
+  {
+    return name;
+  }
+
+  virtual double getDevelopmentCost() const = 0;
+
+  virtual void display(int n) const = 0;
+
+  virtual Iterator *createIterator() {return nullptr;}; // Added for calling DecoratorIterator later
+};
+
+#endif
---
+++ ProductionGroup.h
@@ -0,0 +1,83 @@
+#ifndef PRODUCTION_GROUP_H
+#define PRODUCTION_GROUP_H
+
+#include "ProductionComponent.h"
+#include "Iterator.h"
+#include <vector>
+#include <iostream>
+
+class FullTraversalIterator; //Added foward declare for Iterator
+
+class ProductionGroup : public ProductionComponent
+{
+private:
+  std::vector<ProductionComponent *> children;
+
+  friend class FullTraversalIterator; //Friend class declaration for concreteIterator
+
+public:
+  ProductionGroup(const std::string &name) : ProductionComponent(name) {}
+
+  void addChild(ProductionComponent *child)
+  {
+    if (!child)
+    {
+      std::cerr << "Error: Cannot add a null child to ProductionGroup." << std::endl;
+      return;
+    }
+    children.push_back(child);
+  }
+
+  ProductionComponent *removeChild(const std::string &name)
+  {
+
+    std::vector<ProductionComponent *>::iterator it;
+    for (it = children.begin(); it != children.end(); ++it)
+    {
+      if ((*it)->getName() == name)
+      {
+        ProductionComponent *removedChild = *it;
+        children.erase(it);
+        return removedChild;
+      }
+    }
+    return nullptr; // Child not found
+  }
+
+  double getDevelopmentCost() const override
+  {
+    double totalCost = 0.0;
+    for (const auto &child : children)
+    {
+      totalCost += child->getDevelopmentCost();
+    }
+    return totalCost;
+  }
+
+  void display(int depth) const
+  {
+    std::vector<ProductionComponent *>::const_iterator it;
+
+    std::string indent(depth * 2, ' ');
+    std::cout << indent << "[ProductionGroup[]: " << name << std::endl;
+    for (it = children.begin(); it != children.end(); ++it)
+    {
+      (*it)->display(depth + 1);
+    }
+    std::cout << indent << "]" << std::endl;
+  }
+
+  Iterator *createIterator() override; //method to call FullTraversalIterator
+
+  ~ProductionGroup()
+  {
+    std::vector<ProductionComponent *>::iterator it;
+
+    for (it = children.begin(); it != children.end(); ++it)
+    {
+      delete *it;
+    }
+  }
+};
+
+#endif
