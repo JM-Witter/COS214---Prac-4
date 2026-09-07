c01b1d58b3c05ac11fdde4c8f94dc0d952d19dac
KP23545756 <162136507+KP23545756@users.noreply.github.com>
2026-09-07 11:10:11 +0200

    Full Traversal iterator with accompanying main

---
+++ main.cpp
@@ -0,0 +1,203 @@
+#include <iostream>
+
+#include "Production.h"
+#include "ProductionGroup.h"
+#include "Iterator.h"
+#include "FullTraversalIterator.h"
+
+int main()
+{
+  // root
+  ProductionGroup *starWars =
+      new ProductionGroup("Star Wars");
+
+  // tv series
+  ProductionGroup *tvSeries =
+      new ProductionGroup("TV Series");
+
+  // leaves
+  Production *mandalorian =
+      new Production(
+          "The Mandalorian",
+          "Lucasfilm",
+          15000000.0,
+          24);
+
+  Production *bobaFett =
+      new Production(
+          "The Book of Boba Fett",
+          "Lucasfilm",
+          12000000.0,
+          7);
+
+  Production *acolyte =
+      new Production(
+          "The Acolyte",
+          "Lucasfilm",
+          10000000.0,
+          8);
+
+  // Add information to Mandalorian
+  mandalorian->addCastMember("Pedro Pascal");
+  mandalorian->addCastMember("Giancarlo Esposito");
+
+  mandalorian->setCredits(
+      "Created by Jon Favreau");
+
+  // Add leaves to TV Series composite
+  tvSeries->addChild(mandalorian);
+  tvSeries->addChild(bobaFett);
+  tvSeries->addChild(acolyte);
+
+  // animated series, inside tv series
+  ProductionGroup *animatedSeries =
+      new ProductionGroup("Animated TV Series");
+
+  animatedSeries->addChild(
+      new Production(
+          "The Clone Wars",
+          "Lucasfilm Animation",
+          8500000.0,
+          133));
+
+  animatedSeries->addChild(
+      new Production(
+          "Rebels",
+          "Lucasfilm Animation",
+          6000000.0,
+          75));
+
+  animatedSeries->addChild(
+      new Production(
+          "Resistance",
+          "Lucasfilm Animation",
+          3500000.0,
+          40));
+
+  // Add Composite to another Composite
+  tvSeries->addChild(animatedSeries);
+
+  // movies
+  ProductionGroup *movies =
+      new ProductionGroup("Movies");
+
+  movies->addChild(
+      new Production(
+          "The Empire Strikes Back",
+          "Lucasfilm",
+          18000000.0));
+
+  movies->addChild(
+      new Production(
+          "The Force Awakens",
+          "Lucasfilm",
+          245000000.0));
+
+  // build tree
+  starWars->addChild(tvSeries);
+  starWars->addChild(movies);
+
+  // display tree
+  std::cout << "STAR WARS PRODUCTIONS" << std::endl;
+
+  starWars->display(0);
+
+  // full traversal via Iterator
+  std::cout << "\nFULL TRAVERSAL (Iterator pattern)" << std::endl;
+
+  Iterator *it = starWars->createIterator();
+  for (it->first(); !it->isDone(); it->next())
+  {
+    std::cout << " - " << it->currentItem()->getName() << std::endl;
+  }
+  std::cout << std::endl;
+  delete it;
+
+  // developmen costs
+  std::cout << std::fixed;
+
+  std::cout << "DEVELOPMENT COSTS" << std::endl;
+
+  std::cout
+      << "The Mandalorian: R"
+      << mandalorian->getDevelopmentCost()
+      << std::endl;
+
+  std::cout
+      << "Animated Series: R"
+      << animatedSeries->getDevelopmentCost()
+      << std::endl;
+
+  std::cout
+      << "TV Series: R"
+      << tvSeries->getDevelopmentCost()
+      << std::endl;
+
+  std::cout
+      << "Movies: R"
+      << movies->getDevelopmentCost()
+      << std::endl;
+
+  std::cout
+      << "Entire Star Wars hierarchy: R"
+      << starWars->getDevelopmentCost()
+      << std::endl;
+
+  // adding a leaf
+  std::cout << "ADDING ROGUE ONE" << std::endl;
+
+  movies->addChild(
+      new Production(
+          "Rogue One",
+          "Lucasfilm",
+          200000000.0));
+
+  std::cout << "\nUpdated hierarchy:" << std::endl;
+
+  starWars->display(0);
+
+  std::cout
+      << "\nUpdated total cost: R"
+      << starWars->getDevelopmentCost()
+      << std::endl;
+
+  // removing
+  std::cout << "REMOVING THE FORCE AWAKENS" << std::endl;
+
+  ProductionComponent *removed =
+      movies->removeChild("The Force Awakens");
+
+  if (removed != nullptr)
+  {
+    std::cout
+        << removed->getName()
+        << " removed successfully."
+        << std::endl;
+
+    // removeChild removed it from the Composite,
+    // so movies no longer owns it.
+    delete removed;
+    removed = nullptr;
+  }
+  else
+  {
+    std::cout
+        << "Production was not found."
+        << std::endl;
+  }
+
+  std::cout << "\nFinal hierarchy:" << std::endl;
+
+  starWars->display(0);
+
+  std::cout
+      << "\nFinal total cost: R"
+      << starWars->getDevelopmentCost()
+      << std::endl;
+
+  delete starWars;
+
+  starWars = nullptr;
+
+  return 0;
+}
